77de91ed62c85a29cf4f4b397b6544e545af48d6
Ammar Ahmed <ammar7298@gmail.com>
2024-08-15 12:15:10 +0500

    new changes

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import Image from "next/image";
-import './LandingPage1.css';
+import styles from './LandingPage1.module.css';
 
 const LandingPage = () => {
   return (
     <div className='bg-black'>
-      <header className="header">
-        <a href="#" className="logo">
+      <header className={styles.header}>
+        <a href="#" className={styles.logo}>
           Complex <span>Cad</span>
         </a>
-        <div className="menu">
+        <div className={styles.menu}>
           <i className="fa fa-bars" aria-hidden="true"></i>
         </div>
-        <nav className="navbar">
+        <nav className={styles.navbar}>
           <a href="#Home">Home</a>
         </nav>
       </header>
 
-      <section className="home" id="Home">
-        <div className="content">
+      <section className={styles.home} id="Home">
+        <div className={styles.content}>
           <h1>
             Hi, it's <span>Hasham Rana</span>
           </h1>
-          <h3 className="text-animation">
+          <h3 className={styles.textAnimation}>
             I specialize in <span></span>
           </h3>
           <p>
             With expertise in 2D Civil Map Making, Draft Creation, and the transformation of sketches or paper maps into precise digital drafts,
             I offer comprehensive AutoCAD services for architectural and civil engineering projects.
           </p>
-          <div className="icons">
+          <div className={styles.icons}>
             <a href="https://www.instagram.com/complex_cad/?igsh=M29jdmVkYWR5cHdy">
               <i className="fab fa-instagram" aria-hidden="true"></i>
             </a>
           </div>
-          <div className="flex-btn">
-            <a href="#" className="btn hire">
+          <div className={styles.flexBtn}>
+            <a href="#" className={`${styles.btn} ${styles.hire}`}>
               Hire Me
             </a>
-            <a href="#" className="btn">
+            <a href="#" className={styles.btn}>
               Contact
             </a>
           </div>
@@ -46,9 +46,10 @@ const LandingPage = () => {
         <Image
           src="/profile.png"
           alt="Profile Image"
-          className="img"
+          className={styles.img}
           width={720}
           height={720}
+          unoptimized={true} // Optional if needed
         />
       </section>
     </div>
