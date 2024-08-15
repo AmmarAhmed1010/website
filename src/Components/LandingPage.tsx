0357b1f26bc4e1ae3e73b4f1311cde6e05265d2e
Ammar Ahmed <ammar7298@gmail.com>
2024-08-15 12:24:24 +0500

    newt

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import Image from "next/image";
-import styles from './LandingPage1.module.css';
+import './LandingPage1.css';
 
 const LandingPage = () => {
   return (
     <div className='bg-black'>
-      <header className={styles.header}>
-        <a href="#" className={styles.logo}>
+      <header className="header">
+        <a href="#" className="logo">
           Complex <span>Cad</span>
         </a>
-        <div className={styles.menu}>
+        <div className="menu">
           <i className="fa fa-bars" aria-hidden="true"></i>
         </div>
-        <nav className={styles.navbar}>
+        <nav className="navbar">
           <a href="#Home">Home</a>
         </nav>
       </header>
 
-      <section className={styles.home} id="Home">
-        <div className={styles.content}>
+      <section className="home" id="Home">
+        <div className="content">
           <h1>
             Hi, it's <span>Hasham Rana</span>
           </h1>
-          <h3 className={styles.textAnimation}>
+          <h3 className="text-animation">
             I specialize in <span></span>
           </h3>
           <p>
             With expertise in 2D Civil Map Making, Draft Creation, and the transformation of sketches or paper maps into precise digital drafts,
             I offer comprehensive AutoCAD services for architectural and civil engineering projects.
           </p>
-          <div className={styles.icons}>
+          <div className="icons">
             <a href="https://www.instagram.com/complex_cad/?igsh=M29jdmVkYWR5cHdy">
               <i className="fab fa-instagram" aria-hidden="true"></i>
             </a>
           </div>
-          <div className={styles.flexBtn}>
-            <a href="#" className={`${styles.btn} ${styles.hire}`}>
+          <div className="flex-btn">
+            <a href="#" className="btn hire">
               Hire Me
             </a>
-            <a href="#" className={styles.btn}>
+            <a href="#" className="btn">
               Contact
             </a>
           </div>
@@ -46,10 +46,9 @@ const LandingPage = () => {
         <Image
           src="/profile.png"
           alt="Profile Image"
-          className={styles.img}
+          className="img"
           width={720}
           height={720}
-          unoptimized={true} // Optional if needed
         />
       </section>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Cards3 from "@/Components/Cards3";
 import LandingPage from "@/Components/LandingPage";
 import Image from "next/image";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import "./globals.css";
+
 export default function Home() {
   return (
    <>
